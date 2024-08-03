7098be9b86313b85960610331c73ced246a1c128
jochen <jochen.heckl@gmail.com>
2024-08-03 12:35:29 +0200

    Lerning Go...

--- Sqrt/main.go
+++ Sqrt/main.go
@@ -8,7 +8,7 @@ import (
 func ImproveGuess(argument float64, bestGuess float64) (bool, float64) {
 	nextGuess := (bestGuess + (argument / bestGuess)) / float64(2.0)
 
-	fmt.Printf("Improving guess from %T %v to %T %v", bestGuess, bestGuess, nextGuess, nextGuess)
+	fmt.Printf("Improving guess from %v to %v\n", bestGuess, nextGuess)
 
 	delta := math.Abs(nextGuess - bestGuess)
 	return (delta < float64(math.SmallestNonzeroFloat32)), nextGuess
@@ -18,17 +18,17 @@ func Sqrt(argument float64) {
 	isDone := false
 	guess := argument * 0.5
 
-	fmt.Printf("Calculating square root of %v.", argument)
+	fmt.Printf("Calculating square root of %v.\n", argument)
 
 	for !isDone {
 		isDone, guess = ImproveGuess(argument, guess)
-		fmt.Println("")
 	}
 
-	fmt.Printf("The square root of %v ≈ %v (%v)", argument, guess, math.Sqrt(argument))
+	fmt.Printf("The square root of %v ≈ %v (%v)\n", argument, guess, math.Sqrt(argument))
 }
 
 func main() {
-
 	Sqrt(10)
+	Sqrt(100)
+	Sqrt(1000000000000)
 }
